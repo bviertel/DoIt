23a061df2d3fc384f89ab8278953e8e16dfdde9e
Brandon Viertel <bviertel@gmail.com>
2017-04-12 09:55:00 -0400

    CHANGE: Created ‘Task’ class

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Ann Marie Seyerlein on 4/12/17.
+//  Copyright © 2017 Brandon. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name : String = ""
+    var important : Bool = false
+    
+}
