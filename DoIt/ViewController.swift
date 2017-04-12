024921df9d9ff9749093bfe756bc737b7cbdf298
Brandon Viertel <bviertel@gmail.com>
2017-04-12 10:06:41 -0400

    CHANGE: Added tasks and set importance with ‘!’

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // Table View Variable
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do this before dataSource and Delegate
+        tasks = makeTasks()
         
         // Where does it get it's data from
         tableView.dataSource = self
@@ -28,7 +33,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // Number of rows in Table, 'numberOfRowsInSection'
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return tasks.count
         
     }
     
@@ -38,12 +43,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // New Constant 'cell' of class 'UITableViewCell'
         let cell = UITableViewCell()
         
-        // Sets value of all cells to 'Hello'
-        cell.textLabel!.text = "Hello"
+        let task = tasks[indexPath.row]
+        
+        // If the task.important value is true, add exclamation points to beginning
+        
+        if task.important {
+            
+            cell.textLabel!.text = "❗️ \(task.name)"
+            
+        } else {
+            
+            cell.textLabel!.text = task.name
+            
+        }
         
         // Returns the constant 'cell'
         return cell
     
+    }
+    
+    func makeTasks() -> [Task] {
+        
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+
+        return [task1,task2,task3]
+        
     }
 
 }
