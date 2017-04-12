4f653c71f5c3809b5c36c3d728bc0594a6e5e275
Brandon Viertel <bviertel@gmail.com>
2017-04-12 09:52:10 -0400

    CHANGE: Added Table View

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Inserted UITableViewDelegate and UITableViewDataSource
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // Table View Variable
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Where does it get it's data from
+        tableView.dataSource = self
+        
+        tableView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Number of rows in Table, 'numberOfRowsInSection'
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+        
     }
-
+    
+    // Information in cells, 'cellForRowAt'
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // New Constant 'cell' of class 'UITableViewCell'
+        let cell = UITableViewCell()
+        
+        // Sets value of all cells to 'Hello'
+        cell.textLabel!.text = "Hello"
+        
+        // Returns the constant 'cell'
+        return cell
+    
+    }
 
 }
 
